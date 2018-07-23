2747714d16cc7dcaee779522a24adf5607624ba4
Pico143 <pico19942@gmail.com>
2018-07-23 10:47:05 +0200

    added parsing of json

--- player.py
+++ player.py
@@ -1,8 +1,10 @@
+import json
 
 class Player:
     VERSION = "The easily winning poker robot V1"
-
+     
     def betRequest(self, game_state):
+        gameStateObject = json.load(game_state)
         return 500000
 
     def showdown(self, game_state):
