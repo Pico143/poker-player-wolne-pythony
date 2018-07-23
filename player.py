9d3b253a2db5869785672efebb89916cd81d1b2e
Pico143 <pico19942@gmail.com>
2018-07-23 14:08:22 +0200

    major code change; added checking game state

--- player.py
+++ player.py
@@ -3,33 +3,34 @@ import traceback
 
 class Player:
     VERSION = "The easily winning poker robot V6"
-     
+
     def betRequest(self, game_state):
+        index = game_state['in_action']
         try:
-            if self.evaluateHand(game_state):
-                return 200
-            else:
-                return 0
+            if self.checkPairs(game_state):
+                return 99999
+            if self.checkFigures(game_state):
+                return game_state['current_buy_in'] - game_state['players'][index]['bet'] + 11
         except:
             traceback.print_exc()
-            return 500
+            return 999999
 
     def showdown(self, game_state):
         pass
 
-    def evaluateHand(self, game_state):
+    def checkPairs(self, game_state):
         playersList = game_state['players']
         for player in playersList:
             if player['name'] == "Wolne Pythony":
-                print "NAME WAS WOLNE PYTHONY"
-                print player['hole_cards'][0]['rank']
-                print player['hole_cards'][1]['rank']
                 if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank']:
-                    print "RANKS WERE THE SAME"
                     return True
+
+    def checkFigures(self, game_state):
+        playersList = game_state['players']
+        for player in playersList:
+            if player['name'] == "Wolne Pythony":
                 for card in player['hole_cards']:
                     if card['rank'] in ["A","K","Q","J"]:
-                        print "WE HAD A FIGURE"
                         return True
         return False
 
