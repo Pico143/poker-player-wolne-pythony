e5fee4aa463d40ee1c1e7835ad6c10d38080a362
Pico143 <pico19942@gmail.com>
2018-07-23 11:50:52 +0200

    test version with prints

--- player.py
+++ player.py
@@ -16,10 +16,14 @@ class Player:
         playersList = gameStateObject.players
         for player in playersList:
             if player.name == "Wolne Pythony":
+                print "NAME WAS WOLNE PYTHONY"
+                print player.hole_cards
                 if player.hole_cards[0].rank == player.hole_cards[1].rank:
+                    print "RANKS WERE THE SAME"
                     return True
                 for card in player.hole_cards:
                     if card.rank in ["A","K","Q","J"]:
+                        print "WE HAD A FIGURE"
                         return True
         return False
 
