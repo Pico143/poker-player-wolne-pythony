9fa57e6dd44de1a2be7f486e98643334c7d8b58f
lukibrzoza <lukibrzoza@gmail.com>
2018-07-23 16:33:18 +0200

    fast fix

--- player.py
+++ player.py
@@ -10,9 +10,8 @@ class Player:
         try:
             if self.checkHigherPairs(game_state) or self.top10Hand(game_state, playerIndex):
                 return 99999
-            if self.checkFigures(game_state) or self.checkLowerPairs(game_state):
-                if current_buy_in - players[playerIndex]['bet'] <= 300:
-                    return current_buy_in - players[playerIndex]['bet']
+            if self.checkFigures(game_state) or checkLowerPairs(game_state):
+                pass
             return 0
         except:
             traceback.print_exc()
@@ -21,18 +20,18 @@ class Player:
     def showdown(self, game_state):
         pass
 
-    def checkLowerPairs(self, game_state):
+    def checkHigherPairs(self, game_state):
         playersList = game_state['players']
         for player in playersList:
             if player['name'] == "Wolne Pythony":
-                if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank'] and player['hole_cards'][0]['rank'] in ["2","3","4","5","6"]:
+                if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank'] and player['hole_cards'][0]['rank'] not in ["2","3","4","5","6"]:
                     return True
 
     def checkHigherPairs(self, game_state):
         playersList = game_state['players']
         for player in playersList:
             if player['name'] == "Wolne Pythony":
-                if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank'] and player['hole_cards'][0]['rank'] not in ["2","3","4","5","6"]:
+                if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank'] and player['hole_cards'][0]['rank'] in ["2","3","4","5","6"]:
                     return True
 
     def checkFigures(self, game_state):
