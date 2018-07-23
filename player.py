ba22ea3a8d9ebacedd253dd4780a7787ac3c7576
Pico143 <pico19942@gmail.com>
2018-07-23 16:27:00 +0200

    push fix

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player:
         playerIndex = game_state['in_action']
         try:
             if self.checkHigherPairs(game_state) or self.top10Hand(game_state, playerIndex):
-                return 999999
+                return 99999
             if self.checkFigures(game_state) or checkLowerPairs(game_state):
                 if current_buy_in - players[playerIndex]['bet'] <= 300:
                     return current_buy_in - players[playerIndex]['bet']
@@ -21,11 +21,18 @@ class Player:
     def showdown(self, game_state):
         pass
 
-    def checkHigherPairs(self, game_state):
+    def checkLowerPairs(self, game_state):
         playersList = game_state['players']
         for player in playersList:
             if player['name'] == "Wolne Pythony":
                 if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank'] and player['hole_cards'][0]['rank'] in ["2","3","4","5","6"]:
+                    return True
+
+    def checkHigherPairs(self, game_state):
+        playersList = game_state['players']
+        for player in playersList:
+            if player['name'] == "Wolne Pythony":
+                if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank'] and player['hole_cards'][0]['rank'] not in ["2","3","4","5","6"]:
                     return True
 
     def checkFigures(self, game_state):
