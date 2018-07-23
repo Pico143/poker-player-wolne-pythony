703aeecf5f2dfdaf6ee858fb464bb8c7474e70c2
Pico143 <pico19942@gmail.com>
2018-07-23 11:52:54 +0200

    removed self from argument list

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
     VERSION = "The easily winning poker robot V3"
      
     def betRequest(self, game_state):
-        if self.evaluateHand(self, game_state):
+        if self.evaluateHand(game_state):
             return 500000
         else:
             return 0
