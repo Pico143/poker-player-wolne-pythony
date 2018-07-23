ae08fcc74fda95421eb0467a3775e76be635e69b
Pico143 <pico19942@gmail.com>
2018-07-23 11:26:52 +0200

    fast fix for non negative

--- player.py
+++ player.py
@@ -4,17 +4,12 @@ class Player:
     VERSION = "The easily winning poker robot V2"
      
     def betRequest(self, game_state):
-        gameStateObject = json.load(game_state)
-        if self.evaluateHand(gameStateObject):
-            return 500000
-        else:
-            return 0
+        return 5000
 
     def showdown(self, game_state):
         pass
 
     def evaluateHand(self, gameStateObject):
-        
         playersList = gameStateObject.players
         for player in playersList:
             if player.name == "Wolne Pythony":
