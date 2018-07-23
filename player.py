3d9b56d7f918f6ada6e9e132dc897e23cd7f32e1
Pico143 <pico19942@gmail.com>
2018-07-23 12:12:45 +0200

    fixed dictionary calls

--- player.py
+++ player.py
@@ -4,7 +4,10 @@ class Player:
     VERSION = "The easily winning poker robot V4"
      
     def betRequest(self, game_state):
-        return 5000
+        if self.evaluateHand(game_state):
+            return 500000
+        else:
+            return 0
 
     def showdown(self, game_state):
         pass
@@ -12,10 +15,10 @@ class Player:
     def evaluateHand(self, game_state):
         playersList = game_state['players']
         for player in playersList:
-            if player.name == "Wolne Pythony":
+            if player['name'] == "Wolne Pythony":
                 print "NAME WAS WOLNE PYTHONY"
                 print player.hole_cards[0]
-                if player.hole_cards[0].rank == player.hole_cards[1].rank:
+                if player.hole_cards[0]['rank'] == player.hole_cards[1]['rank']:
                     print "RANKS WERE THE SAME"
                     return True
                 for card in player.hole_cards:
